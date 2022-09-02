ba025965907d45610b90cbfc7d39f0e05453f375
Emilio <erinaudo1@hotmail.com>
2022-09-02 17:00:26 -0300

    replanteo

--- src/components/pure/ComponentB.jsx
+++ src/components/pure/ComponentB.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Contacto } from '../../models/contact.class';
 
-const ComponentB = ({contacto}) => {
+
+const ComponentB = () => {
     return (
         <div>
-            <h1> - { contacto.nombre } { contacto.apellido } - </h1>
-            <h3>Direccion de email: { contacto.email } </h3>
-            <br/>
-            <h3>Estado: </h3>
-            <h2>{ contacto.conectado ? "Contacto En Línea" : "Contacto No Disponible" } </h2>
+            
         </div>
     );
 };
 
 
 ComponentB.propTypes = {
-    contacto: PropTypes.instanceOf(Contacto)
+
 };
 
 
-export { ComponentB };
+export default ComponentB;
+
