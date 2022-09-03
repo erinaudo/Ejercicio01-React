2445dba96f53df124cf0e83275148dc6fa14ed89
Emilio <erinaudo1@hotmail.com>
2022-09-03 11:41:56 -0300

    Replanteo del enfoque de todo el proyecto

--- src/components/container/ComponentA.jsx
+++ src/components/container/ComponentA.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Contacto } from '../../models/contact.class';
+import ComponentB from '../pure/ComponentB';
 
 
-const ComponentA = () => {
+const ComponentA = ({contact}) => {
+
+    const contacto1 = new Contacto('Emilio','Rinaudo','erinaudo@hotmail.com', true)
+
     return (
         <div>
-            
+            <h1>
+                Tus Contactos
+            </h1>
+            <h2>
+                Nombre: { contacto1.nombre } { contacto1.apellido } -
+            </h2>
+            <h4>
+                Email: { contacto1.email }
+            </h4>
+            <ComponentB></ComponentB>
         </div>
     );
 };
--- src/components/pure/ComponentB.jsx
+++ src/components/pure/ComponentB.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 
-const ComponentB = () => {
+const ComponentB = ({estado}) => {
+    
+    const [conectado, setState] = useState (estado)
+    
     return (
         <div>
-            
+            <p>
+               Estade de Conexión: <br/>
+               <strong>{ conectado === false ? 'Contacto No Disponible' : 'Contacto En Línea' }</strong>
+            </p>
+            <button onClick={() => setState(!conectado)}>
+                {conectado === false ? 'Conectar' : 'Desconectar'}
+            </button>
         </div>
     );
 };
 
 
 ComponentB.propTypes = {
-
+    estado : PropTypes.bool
 };
 
 
