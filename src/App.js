a094a6ae033d8d8d9416288fc6245305f80525d4
Emilio <erinaudo1@hotmail.com>
2022-09-05 12:13:37 -0300

    Elimina comentarios en app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import './App.css';
-import ComponentA from './components/ComponentA'
-import { Contacto } from './models/contact.class';
+import "./App.css";
+import ComponentA from "./components/ComponentA";
+import { Contacto } from "./models/contact.class";
 
 function App() {
-
-    const contacto1 = new Contacto ('Mateo', 'Rinaudo', 'mate@hotmail.com', false)
-/*   const contacto1 = {
-    nombre: 'Emilio',
-    apellido: 'Rinaudo',
-    email: 'erinaudo@hotmail.com',
-    conectato: true
-  } */
+  const contacto1 = new Contacto("Mateo", "Rinaudo", "mate@hotmail.com", false);
 
   return (
     <div className="App">
       <header className="App-header">
-        <ComponentA contacto= { contacto1 } />
+        <ComponentA contacto={contacto1} />
       </header>
     </div>
   );
