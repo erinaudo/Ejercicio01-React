4663ae1d20868eead284d47b8b0c9b99f2b60d74
Emilio <erinaudo1@hotmail.com>
2022-09-05 12:09:32 -0300

    Replanteo 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import ComponentA from './components/ComponentA'
+import { Contacto } from './models/contact.class';
 
 function App() {
+
+    const contacto1 = new Contacto ('Mateo', 'Rinaudo', 'mate@hotmail.com', false)
+/*   const contacto1 = {
+    nombre: 'Emilio',
+    apellido: 'Rinaudo',
+    email: 'erinaudo@hotmail.com',
+    conectato: true
+  } */
+
   return (
     <div className="App">
       <header className="App-header">
-       <ComponentA></ComponentA>
+        <ComponentA contacto= { contacto1 } />
       </header>
     </div>
   );
--- src/components/ComponentA.jsx
+++ src/components/ComponentA.jsx
@@ -1,33 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Contacto } from '../models/contact.class';
-import ComponentB from './ComponentB';
+import React from "react";
+import PropTypes from "prop-types";
+import { Contacto } from "../models/contact.class";
+import ComponentB from "./ComponentB";
 
+const ComponentA = ({ contacto }) => {
+  return (
+    <div>
+      <h1>Estado de Contacto</h1>
+      <h2>
+        Nombre: {contacto.nombre} {contacto.apellido}
+      </h2>
+      <h4>Direccion de email: {contacto.email}</h4>
 
-const ComponentA = ({contact}) => {
-
-    const contacto1 = new Contacto('Emilio','Rinaudo','erinaudo@hotmail.com', true)
-
-    return (
-        <div>
-            <h1>
-                Tus Contactos
-            </h1>
-            <h2>
-                Nombre: { contacto1.nombre } { contacto1.apellido } -
-            </h2>
-            <h4>
-                Email: { contacto1.email }
-            </h4>
-            <ComponentB></ComponentB>
-        </div>
-    );
+      <ComponentB estado={ contacto.estado } />
+    </div>
+  );
 };
-
 
 ComponentA.propTypes = {
-    contact: PropTypes.instanceOf(Contacto)
+  contacto: PropTypes.instanceOf(Contacto),
 };
-
 
 export default ComponentA;
